332cee8eaae7977b93a325104b8051374e595699
DiegoGonzález64 <135925516+DiegoGonzalez64@users.noreply.github.com>
2023-09-26 19:03:35 -0300

    Modificamos el archivo, enciende y apagan Leds y sirena como alarma

--- Codigo_Arduino_Muestra_Temperaturas_Pantalla_OLED/Codigo_Arduino_Muestra_Temperaturas_Pantalla_OLED.ino
+++ Codigo_Arduino_Muestra_Temperaturas_Pantalla_OLED/Codigo_Arduino_Muestra_Temperaturas_Pantalla_OLED.ino
@@ -19,6 +19,20 @@ int a;
 
 //Variable de tipo Thermistor, PIN ANALÓGICO (A3) es adonde el sensor se conecta
 Thermistor temp(3);
+
+//Variables y Pines usados para los Leds
+int ledVerde = 6;
+int ledRojo = 7;
+
+//Variables y Pines usados para la sirena
+int Sirena = 8;                  //Pin de la sirena
+int Cuarto = 1000/4;             //Tiemo de 1/4 de nota
+int Octavo = 1000/8;             //Tiempo de 1/8 de nota
+double pausa = 1.30;             //Pausa del 30% de la nota
+
+int N1 = 264;                    //Frecuencia de la nota 1 es de 264 Hz
+int N2 = 440;                    //Frecuencia de la nota 2 es de 440 Hz
+int N3 = 669;                    //Frecuencia de la nota 3 es de 669 Hz
  
 void setup() {
 #ifdef __DEBUG__
@@ -34,12 +48,18 @@ void setup() {
 #endif
     while (true);
   }
+
+  pinMode(6, OUTPUT);          //Pin 7 salida
+  pinMode(7, OUTPUT);          //Pin 8 salida
+  {
+    pinMode(Sirena, OUTPUT);   //Sirena será un Pin de salida
+  }
   
  
 }
  
 void loop() {
-  float temperature = temp.getTemp(); //Variable entera (int)
+  float temperature = temp.getTemp(); //Variable decimal (float)
   Serial.print("Temperatura: "); //Imprime texto en el Monitor
   Serial.print(temperature); //Imprime la temperatura sensada
   Serial.println("*C");
@@ -48,7 +68,7 @@ void loop() {
   display.clearDisplay();
  
   // Tamaño del texto
-  display.setTextSize(2);
+  display.setTextSize(1);
   // Color del texto
   display.setTextColor(SSD1306_WHITE);
   // Posición del texto
@@ -57,11 +77,17 @@ void loop() {
   display.print(" Temp: "); //Muestra la temperatura sensada
   display.print(temperature);
   display.println(" *C");
+
  
   // Enviar a pantalla
-  display.display();  
-  
+  display.display();
+
   delay(6000); //Lectura cada 6 segundos
+
+  // Mover texto de izquierda a derecha
+  display.startscrollright(0x00, 0x0F);
+  delay(15000);
+  display.stopscroll();
 
   if(Serial.available()){
     c=Serial.read();
@@ -75,4 +101,23 @@ void loop() {
       }
     }
   }
+
+  if (temperature >=25){
+  digitalWrite(7, HIGH);               //si la temperatura es mayor a 25ºC Led rojo prende
+  digitalWrite(6, LOW);                //si la temperatura es mayor a 25ºC Led verde se apaga
+  }
+
+  
+  else if (temperature <=21){
+    digitalWrite(7, LOW);               //si la temperatura es menor o igual a 25ºC Led rojo apaga
+    digitalWrite(6, HIGH);                //si la temperatura es menor o igual a 25ºC Led verde se prende
+
+    tone(Sirena, N1, Cuarto);
+    delay(Cuarto*pausa);
+     tone(Sirena, N2, Octavo);
+    delay(Octavo*pausa);
+     tone(Sirena, N3, Octavo);
+    delay(Octavo*pausa);
+  }
+
 }
